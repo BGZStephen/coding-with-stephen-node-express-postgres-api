68ed637b215998c6846a6ba84c965cc459be269f
Stephen <sjw948@gmail.com>
2021-08-17 18:42:16 +0100

    add finish files for part 4

--- src/app/domain/startup/index.js
+++ src/app/domain/startup/index.js
@@ -1,10 +1,15 @@
 const { validateEnvProvidedConfig } = require("../config")
+const { AppHttpServer } = require("../http-server")
 
 class AppStarter {
-  static startServices() {
+  static async startServices() {
     console.log("Preparing environment variables")
 
     validateEnvProvidedConfig();
+
+    console.log("Starting services for application")
+
+    await AppHttpServer.start()
 
     console.log("All services started for application")
   }
