2ca817f10ecd9e553849880ff516bb3f2ea4ddd3
Stephen <sjw948@gmail.com>
2021-10-07 19:49:52 +0100

    starting files

--- src/app/domain/test-utils/db-queries.js
+++ src/app/domain/test-utils/db-queries.js
@@ -1,7 +1,10 @@
 const { postgres } = require("../db");
 
 async function resetDatabase() {
+  await postgres.client.query(`ALTER TABLE "authentication-tokens" DROP CONSTRAINT "foreign-key-userid-authentication-token"`)
+  await postgres.client.query(`TRUNCATE TABLE "authentication-tokens"`)
   await postgres.client.query("TRUNCATE TABLE users")
+  await postgres.client.query(`ALTER TABLE "authentication-tokens" ADD CONSTRAINT "foreign-key-userid-authentication-token" FOREIGN KEY ("userId") REFERENCES users(id)`)
 }
 
 module.exports = {
